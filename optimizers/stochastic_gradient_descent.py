8969d2f9b681cded3229bb64c990aa9905ec3daa
takeru.abe <takeru.abe@gaiax.com>
2022-02-18 14:25:39 +0900

    stochastic gradient descent

---
+++ optimizers/stochastic_gradient_descent.py
@@ -0,0 +1,12 @@
+import numpy as np
+
+class SGD:
+
+    """確率的勾配降下法（Stochastic Gradient Descent）"""
+
+    def __init__(self, lr=0.01):
+        self.lr = lr
+
+    def update(self, params, grads):
+        for key in params.keys():
+            params[key] -= self.lr * grads[key] 
